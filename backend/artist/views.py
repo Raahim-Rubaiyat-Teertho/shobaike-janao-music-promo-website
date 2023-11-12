31f170c0e6033d4a087a2a9eb1912cd9d6dd49e7
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2023-11-13 02:12:01 +0600

    created basic artist api

---
+++ backend/artist/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ArtistSerializer
+
+from .models import Artist
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the artist index.")
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/artists-list/',
+        'Detail View' : '/<str:pk>',
+        'signup' : '/signup',
+    }
+
+    return Response(api_urls)
+
+@api_view(['GET'])
+def artistList(request):
+    artists = Artist.objects.all()
+    serializer = ArtistSerializer(artists, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def artistDetail(request, pk):
+    artist = Artist.objects.get(name = pk)
+    serializer = ArtistSerializer(artist, many = False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def artistSignup(request):
+    serializer = ArtistSerializer(data=request.data)
+
+    if(serializer.is_valid()):
+        serializer.save()
+
+    return Response(serializer.data)
